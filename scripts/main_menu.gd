3f74a72d7e1e74addf94f7fc24f5994573577d6a
Sudheesh-07 <sudheeshshetty48@gmail.com>
2024-11-20 10:42:20 +0530

    Made a start up menu to the game and aslo gave mobile user controls

---
+++ scripts/main_menu.gd
@@ -0,0 +1,16 @@
+extends Control
+var button_type = null
+@onready var fade_transition: ColorRect = $Fade_transition
+
+func _on_start_pressed() -> void:
+	button_type = "start"
+	$Fade_transition.show()
+	$Fade_transition/Timer.start()
+	$Fade_transition/AnimationPlayer.play("Fade_in")
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_timer_timeout() -> void:
+	if button_type == "start":
+		get_tree().change_scene_to_file("res://scenes/world.tscn") # Replace with function body.
