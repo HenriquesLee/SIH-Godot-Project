aeee48df62bc626999efbb1f94a9549fe1818758
Sudheesh-07 <sudheeshshetty48@gmail.com>
2024-11-29 16:45:25 +0530

    not migrating to flutter made minor changes in file system moved things here and there

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -13,5 +13,5 @@ func _on_quit_pressed() -> void:
 
 func _on_timer_timeout() -> void:
 	if button_type == "start":
-		get_tree().change_scene_to_file("res://scenes/world.tscn")
+		get_tree().change_scene_to_file("res://scenes/ex_a1/ex_a1.tscn")
  # Replace with function body.
