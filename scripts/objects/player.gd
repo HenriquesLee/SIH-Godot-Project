6f1f211741810e0d9cae7e8de0a3d555c6740796
HenriquesLee <146438915+HenriquesLee@users.noreply.github.com>
2024-12-11 18:45:20 +0530

    added arrow to all

--- scripts/objects/player.gd
+++ scripts/objects/player.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 @export var speed: float = 80.0
 
 # Target and Arrow Settings
-@onready var target_object = get_node("../Exc_Guide")  # The object/NPC the arrow will point to
+@onready var target_object = get_node("../chest 1")  # The object/NPC the arrow will point to
 @export var arrow_sprite: Sprite2D  # Sprite2D for the arrow
 @export var rotation_offset: float = -10.0  # Rotation offset for the arrow
 
